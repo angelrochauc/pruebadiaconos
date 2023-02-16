4080899a2f3e27b01ec221b7c588bbcb96cde43d
arios.bssltda1 <arios.bssltda@gmail.com>
2023-02-15 20:38:38 -0500

    arreglito fecha

--- js/form.js
+++ js/form.js
@@ -1,63 +1,67 @@
-function imprimirPagina(){
+function imprimirPagina() {
     window.print();
-}function sumar(){
-  var valor1 = Number(document.getElementById('niños').value);
-  var valor2 = Number(document.getElementById('joven').value);
-  var valor3 = Number(document.getElementById('hora').value);
+}
 
-  var total = valor1 + valor2 + valor3;
+function sumar() {
+    var valor1 = Number(document.getElementById('niños').value);
+    var valor2 = Number(document.getElementById('joven').value);
+    var valor3 = Number(document.getElementById('hora').value);
 
-  document.getElementById('total').value = total;
+    var total = valor1 + valor2 + valor3;
+
+    document.getElementById('total').value = total;
 }
 
 
 
 const fecha = document.getElementById("fecha"),
-servicio = document.getElementById("servicio")
+    servicio = document.getElementById("servicio")
 
-function getCurrentDate () {
-  const currentDate = new Date(),
-  options = { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric' };
-  fecha.innerHTML = currentDate.toLocaleDateString('es', options)
+function getCurrentDate() {
+    const currentDate = new Date(),
+        options = { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric' };
+    fecha.innerHTML = currentDate.toLocaleDateString('es', options)
 }
 
-function getCurrentTime () {
-  const currentDate = new Date(),
-  hours = currentDate.getHours(),
-  minutes = formatTime(currentDate.getMinutes()),
-  seconds = formatTime(currentDate.getSeconds()),
-  formatHours = formatTime(((hours + 11) % 12 + 1)),
-  format = (hours < 12) || (hours == 24)  ? 'AM' : 'PM'
-  servicio.innerHTML = `${formatHours}:${minutes}:${seconds} <small>${format}</small>`
-  
+function getCurrentTime() {
+    const currentDate = new Date(),
+        hours = currentDate.getHours(),
+        minutes = formatTime(currentDate.getMinutes()),
+        seconds = formatTime(currentDate.getSeconds()),
+        formatHours = formatTime(((hours + 11) % 12 + 1)),
+        format = (hours < 12) || (hours == 24) ? 'AM' : 'PM'
+    servicio.innerHTML = `${formatHours}:${minutes}:${seconds} <small>${format}</small>`
+
 }
 
-function formatTime (value)  {
-  return value < 10 ? `0${value}` : value
+function formatTime(value) {
+    return value < 10 ? `0${value}` : value
 }
 
 setInterval(getCurrentTime, 1000)
 
 getCurrentDate()
 document.querySelector("#submit").addEventListener("click", e => {
-  e.preventDefault();
+    e.preventDefault();
 
-  //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
-  const telefono = "573104866092";
+    //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+    const telefono = "573104866092";
 
-  const cliente = document.querySelector("#niños").value;
-  const joven = document.querySelector("#joven").value;
-  const total = document.querySelector("#total").value;
-  const fecha = document.querySelector("#fecha").value;
-  const hora = document.querySelector("#hora").value;
-  const empleado = document.querySelector("#empleado").value;
-  const servicio = document.querySelector("#servicio").value;
-  const resp = document.querySelector("#respuesta");
+    const cliente = document.querySelector("#niños").value;
+    const joven = document.querySelector("#joven").value;
+    const total = document.querySelector("#total").value;
+    const fecha = document.querySelector("#fecha").innerHTML;
+    const hora = document.querySelector("#hora").innerText;
+    const empleado = document.querySelector("#empleado").value;
+    const servicio = document.querySelector("#servicio").innerText;
+    const resp = document.querySelector("#respuesta");
 
-  resp.classList.remove("fail");
-  resp.classList.remove("send");
+    resp.classList.remove("fail");
+    resp.classList.remove("send");
 
-  const url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+    console.log(hora);
+
+    const url = `https://api.whatsapp.com/send?phone=${telefono}&text=
 		*Asistencia*%0A
 		*Niños:*  
 		${cliente}%0A
@@ -74,18 +78,16 @@ document.querySelector("#submit").addEventListener("click", e => {
 		*Servicio:* 
 		${servicio}`;
 
-  if (cliente === "" || fecha === "" || hora === "" || joven === "") {
-    resp.classList.add("fail");
-    resp.innerHTML = `Faltan algunos datos`;
-    return false;
-  }
-  resp.classList.remove("fail");
-  resp.classList.add("send");
-  resp.innerHTML = `Se ha enviado`;
+    if (cliente === "" || joven === "") {
+        resp.classList.add("fail");
+        resp.innerHTML = `Faltan algunos datos`;
+        return false;
+    }
+    resp.classList.remove("fail");
+    resp.classList.add("send");
+    resp.innerHTML = `Se ha enviado`;
 
 
 
-  window.open(url);
+    window.open(url);
 });
-
-
